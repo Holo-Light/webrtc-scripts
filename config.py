613daa96f54146b1bffbff0bb7904f002e6772ef
holomarkus <m.webel@holo-light.com>
2019-01-09 10:44:58 +0100

    Included unity plugin in build

--- config.py
+++ config.py
@@ -189,7 +189,8 @@ TARGETS_TO_BUILD = {
                                               [ 
                                                 'webrtc',
                                                 'third_party/jsoncpp:jsoncpp',
-                                                'rtc_base:rtc_json'
+                                                'rtc_base:rtc_json',
+												'examples:webrtc_unity_plugin'
                                               ],1
                                              ),                                            
                                 'ortc'    : (
